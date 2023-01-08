5b1209d1e689802319f7bcc0b7a07fcab751273a
Equinox <ttrrojann@gmail.com>
2023-01-08 18:07:29 +0200

    Task is done

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String>strings = new ArrayList<>();
+        strings.add("aaa");
+        strings.add("aaab");
+        strings.add("aaabb");
+        strings.add("aaabbb");
+        strings.add("aaab");
+        strings.add("aaa");
+        strings.add("aaa");
+        strings.add("aaab");
+        strings.add("aaabb");
+        makeTable(strings, 4);
+    }
+
+    public static void makeTable(List<String> strings, int columnCount) {
+        if (columnCount < 1 || strings.stream().mapToInt(String::length).max().isEmpty()) return;
+        int maxSize = strings.stream().mapToInt(String::length).max().getAsInt()+5;
+        int counter = 0;
+        for(String str: strings){
+            counter++;
+            if(counter==columnCount) {
+                System.out.println(String.format("%"+maxSize+"s", str));
+                counter = 0;
+                continue;
+            }
+            System.out.print(String.format("%"+maxSize+"s", str));
+        }
+
+    }
+}
